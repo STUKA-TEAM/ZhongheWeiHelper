576f6fcad6a74e35f24ec08ccebc7cb14c2a9f87
BelieveIt <374344282@qq.com>
2013-11-27 23:36:09 +0800

    精简图片上传使用

--- WebContent/js/common/tools.js
+++ WebContent/js/common/tools.js
@@ -6,3 +6,27 @@ function getRootPath(){
     var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
     return(localhostPaht+projectName);
 }
+function uploadImg() {
+	var myFiles = new Array();
+	var swfpath = getRootPath() + '/js/common/uploadify.swf';
+	var uploadPath = getRootPath() + '/tools/upload';
+	var dataFormServer = '';
+	var myFileCnt = 0;
+	$('#file-upload').uploadify({
+	'fileTypeDesc' : '图片文件',
+	'fileTypeExts' : '*.jpg; *.png',
+	'fileSizeLimit' : '2000KB',
+	'width' : 70,
+	'height' : 28,
+	'swf': swfpath,
+	'uploader': uploadPath,
+	'buttonText': '上传图片',
+	'multi' : false,
+	'auto' : true,
+	'fileObjName' : 'file',
+	'checkExisting' : false,
+	'onUploadSuccess' : function(file, data, response) {		
+		$("#imgPathID").val(JSON.parse(data).link);
+	}
+	});
+}
