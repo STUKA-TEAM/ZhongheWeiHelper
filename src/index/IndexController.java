501d734550fd349ad179ce42ea7491ea7ceb2f86
BelieveIt <374344282@qq.com>
2013-12-03 23:08:56 +0800

    更新数据库

--- src/index/IndexController.java
+++ src/index/IndexController.java
@@ -1,14 +1,11 @@
 package index;
 
-import java.io.ByteArrayInputStream;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,14 +17,15 @@ import tools.WeixinXMLTools;
 public class IndexController {
 @RequestMapping(value = "/shiyan")
 @ResponseBody
-public String responseWeixin(@RequestParam(value = "signature") String signature,
-		@RequestParam(value = "timestamp") String timestamp,
-		@RequestParam(value = "nonce") String nonce,
-		@RequestParam(value = "echostr") String echostr,
-		@RequestBody String xml){
+public String responseWeixin(@RequestParam(value = "signature", required=false) String signature,
+		@RequestParam(value = "timestamp",required=false) String timestamp,
+		@RequestParam(value = "nonce",required=false) String nonce,
+		@RequestParam(value = "echostr",required=false) String echostr,
+		HttpServletRequest request
+	){
 	SAXReader reader = new SAXReader();
 	try {
-		Document document = reader.read(new ByteArrayInputStream(xml.getBytes()));
+		Document document = reader.read(request.getInputStream());
 		if(document.selectSingleNode("MsgType").getText().equals("event") && 
 					document.selectSingleNode("Event").getText().equals("subscribe")){
 			
@@ -38,7 +36,7 @@ public String responseWeixin(@RequestParam(value = "signature") String signature
 		}
 		
 		return "";
-	} catch (DocumentException e) {
+	} catch (Exception e) {
 		return "";
 	}
 	
